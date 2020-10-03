47b0144689f6d1ba45829e4a8dbc2cf116b1de96
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-10-02 22:32:10 -0400

    created a card per team member

--- team-builder/src/Components/Members.js
+++ team-builder/src/Components/Members.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import {Col,Card} from 'reactstrap';
 
 const Members = (props) =>{
-    console.log(props)
+    console.log(props.members)
     return (
-        <div>Members here</div>
+        <div>
+            {props.members.map(member=>{
+                return <Col sm="5">
+                    <Card>
+                    {member.name}
+                    </Card>
+                </Col>
+            })}
+        </div>
     )
 }
 
